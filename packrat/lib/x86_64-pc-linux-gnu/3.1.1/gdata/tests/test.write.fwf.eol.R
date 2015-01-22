100ba50a8dc0ea8801858ad4aae872148a9b11ce
Joao Teixeira <joaopteixeira58@gmail.com>
2015-01-22 21:05:43 +0000

    master: Added packrat files.

--- packrat/lib/x86_64-pc-linux-gnu/3.1.1/gdata/tests/test.write.fwf.eol.R
+++
@@ -1,5 +0,0 @@
-library(gdata)
-saveto <- tempfile(pattern = "test.txt", tmpdir = tempdir())
-
-write.fwf(x = data.frame(a=1:length(LETTERS), b=LETTERS),
-          file=saveto, eol="\r\n")
--- packrat/lib/x86_64-pc-linux-gnu/3.1.1/lava/tests/test-all.R
+++
@@ -1,6 +0,0 @@
-if (require(testthat)) {
-  library(lava)
-  if (exists("test_check"))
-      test_check("lava")
-}
-
